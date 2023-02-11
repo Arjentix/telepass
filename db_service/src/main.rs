a5a7712312dd98b0ea1872a173930573538aa7c2
Daniil Polyakov <arjentix@gmail.com>
2023-02-11 17:25:15 +0300

    Add tls configuration

--- db_service/src/main.rs
+++ db_service/src/main.rs
@@ -1,7 +1,19 @@
 //! Telepass Database Service to store and retrieve passwords.
 
+// Triggers on `base64` crate.
+//
+// TODO: Remove this once `tonic` uploads a new version to crates.io,
+// because it's fixed on GitHub.
+#![allow(clippy::multiple_crate_versions)]
+
+use std::{fs, path::PathBuf};
+
+use color_eyre::{eyre::WrapErr as _, Result};
 use grpc::database_service_server::DatabaseServiceServer;
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{
+    transport::{Certificate, Identity, Server, ServerTlsConfig},
+    Request, Response, Status,
+};
 
 pub mod grpc {
     //! Module with generated `gRPC` code.
@@ -50,10 +62,27 @@ impl grpc::database_service_server::DatabaseService for DatabaseService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let service = DatabaseService::default();
+
+    let tls_dir = PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "..", "tls"]);
+    let cert = fs::read_to_string(tls_dir.join("server.pem"))
+        .wrap_err("Failed to read server certificate")?;
+    let key = fs::read_to_string(tls_dir.join("server.key"))
+        .wrap_err("Failed to read server private key")?;
+    let server_identity = Identity::from_pem(cert, key);
+
+    let client_ca_cert = std::fs::read_to_string(tls_dir.join("client.pem"))
+        .wrap_err("Failed to read client authentication certificate")?;
+    let client_ca_cert = Certificate::from_pem(client_ca_cert);
+
+    let tls_config = ServerTlsConfig::new()
+        .identity(server_identity)
+        .client_ca_root(client_ca_cert);
+
+    let db_service = DatabaseServiceServer::new(DatabaseService::default());
 
     Server::builder()
-        .add_service(DatabaseServiceServer::new(service))
+        .tls_config(tls_config)?
+        .add_service(db_service)
         .serve(addr)
         .await?;
 
