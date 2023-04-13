7f6484da68ab74afeb1633d608b30e07f3e4e532
Daniil Polyakov <arjentix@gmail.com>
2023-04-13 16:07:54 +0300

    Implement hello-world tg bot

--- telegram_gate/src/main.rs
+++ telegram_gate/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::{eyre::WrapErr as _, Result};
+use dotenvy::dotenv;
+use teloxide::prelude::*;
+use tracing::{info, Level};
+use tracing_subscriber::{filter::LevelFilter, EnvFilter, FmtSubscriber};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    init_logger().wrap_err("Failed to initialize logger")?;
+
+    info!("Hello from Telepass Telegram Gate!");
+
+    dotenv().wrap_err("Failed to load `.env` file")?;
+
+    let bot = Bot::from_env();
+
+    #[allow(clippy::shadow_unrelated)]
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
+
+    Ok(())
+}
+
+/// Initialize logger.
+fn init_logger() -> Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).wrap_err("Failed to set global logger")
 }
