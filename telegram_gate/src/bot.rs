c038703306be46f21d7e6cfbc6d03b67d4249a8c
Daniil Polyakov <arjentix@gmail.com>
2023-08-14 02:15:51 +0300

    Add mocking for `reply_markup()` and `user()`

--- telegram_gate/src/bot.rs
+++ telegram_gate/src/bot.rs
@@ -1,8 +1,13 @@
 //! Module with trait to replace direct [`Bot`](teloxide::Bot) usage.
 //! Used for unit-tests.
 
+#![allow(clippy::module_name_repetitions)]
+#![cfg_attr(test, allow(clippy::indexing_slicing))] // From `automock` macro expansion
+
 use std::future::{Future, IntoFuture};
 
+#[cfg(test)]
+pub use helper_types::*;
 #[cfg(test)]
 use mockall::automock;
 
@@ -13,23 +18,21 @@ use mockall::automock;
 ///
 /// [`Bot`]: teloxide::Bot
 #[cfg_attr(test, automock(
-    type Err=mock::Err;
+    type Error = MockError;
 
-    type Message=mock::Message;
-    type MessageFuture = mock::MessageFuture;
-    type SendMessage=mock::SendMessage;
+    type Message = MockMessage;
+    type MessageFuture = MockMessageFuture;
+    type SendMessage = MockSendMessage;
 
-    type Me=mock::Me;
-    type MeFuture = mock::MeFuture;
-    type GetMe=mock::GetMe;
+    type Me = MockMe;
+    type MeFuture = MockMeFuture;
+    type GetMe = MockGetMe;
 ))]
-#[allow(clippy::module_name_repetitions)]
-#[cfg_attr(test, allow(clippy::indexing_slicing))] // From `automock` macro expansion
 pub trait BotTrait {
-    type Err: std::error::Error + Send + Sync;
+    type Error: std::error::Error + Send + Sync;
 
     type Message;
-    type MessageFuture: Future<Output = Result<Self::Message, Self::Err>> + Send;
+    type MessageFuture: Future<Output = Result<Self::Message, Self::Error>> + Send;
     type SendMessage: IntoFuture<IntoFuture = Self::MessageFuture> + MessageSetters + Send;
 
     fn send_message<C, T>(&self, chat_id: C, message: T) -> Self::SendMessage
@@ -38,16 +41,19 @@ pub trait BotTrait {
         T: Into<String> + 'static;
 
     type Me: MeGetters + Send;
-    type MeFuture: Future<Output = Result<Self::Me, Self::Err>> + Send;
+    type MeFuture: Future<Output = Result<Self::Me, Self::Error>> + Send;
     type GetMe: IntoFuture<IntoFuture = Self::MeFuture> + Send;
 
     fn get_me(&self) -> Self::GetMe;
 }
 
+#[cfg(test)]
+pub type MockBot = MockBotTrait;
+
 pub trait MessageSetters {
     fn reply_markup<T>(self, value: T) -> Self
     where
-        T: Into<teloxide::types::ReplyMarkup>;
+        T: Into<teloxide::types::ReplyMarkup> + 'static;
 }
 
 pub trait MeGetters {
@@ -55,7 +61,7 @@ pub trait MeGetters {
 }
 
 impl BotTrait for teloxide::Bot {
-    type Err = teloxide::errors::RequestError;
+    type Error = teloxide::errors::RequestError;
 
     type Message = teloxide::types::Message;
     type MessageFuture = <Self::SendMessage as IntoFuture>::IntoFuture;
@@ -81,7 +87,7 @@ impl BotTrait for teloxide::Bot {
 impl MessageSetters for teloxide::requests::JsonRequest<teloxide::payloads::SendMessage> {
     fn reply_markup<T>(self, value: T) -> Self
     where
-        T: Into<teloxide::types::ReplyMarkup>,
+        T: Into<teloxide::types::ReplyMarkup> + 'static,
     {
         <Self as teloxide::payloads::SendMessageSetters>::reply_markup(self, value)
     }
@@ -94,73 +100,62 @@ impl MeGetters for teloxide::types::Me {
 }
 
 #[cfg(test)]
-mod mock {
+mod helper_types {
+    //! Module with helper mock types for [`MockBot`](super::MockBot).
+
     use std::future::{ready, Future, IntoFuture, Ready};
+
+    use mockall::mock;
 
     use super::{MeGetters, MessageSetters};
 
-    pub type Err = std::convert::Infallible;
+    pub type MockError = std::convert::Infallible;
 
-    pub type Message = ();
-    pub type MessageFuture = Ready<Result<Message, Err>>;
+    pub type MockMessage = ();
+    pub type MockMessageFuture = Ready<Result<MockMessage, MockError>>;
 
-    #[derive(Default)]
-    pub struct SendMessage;
+    pub type MockMeFuture = Ready<Result<MockMe, MockError>>;
 
-    impl IntoFuture for SendMessage {
-        type Output = <Self::IntoFuture as Future>::Output;
+    // Using `mock!` only for trait which is usefull to check in tests
+    mock! {
+        pub SendMessage {}
 
-        type IntoFuture = MessageFuture;
+        impl MessageSetters for SendMessage {
+            fn reply_markup<T>(self, value: T) -> Self
+            where
+                T: Into<teloxide::types::ReplyMarkup> + 'static;
+        }
+    }
+
+    impl IntoFuture for MockSendMessage {
+        type Output = <MockMessageFuture as Future>::Output;
+
+        type IntoFuture = MockMessageFuture;
 
         fn into_future(self) -> Self::IntoFuture {
             ready(Ok(()))
         }
     }
 
-    impl MessageSetters for SendMessage {
-        fn reply_markup<T>(self, _value: T) -> Self
-        where
-            T: Into<teloxide::types::ReplyMarkup>,
-        {
-            self
+    // Using `mock!` only for trait which is usefull to check in tests
+    mock! {
+        pub Me {}
+
+        impl MeGetters for Me {
+            fn user(&self) -> &teloxide::types::User;
         }
     }
-
-    pub struct Me(teloxide::types::User);
-
-    impl Default for Me {
-        fn default() -> Self {
-            Self(teloxide::types::User {
-                id: teloxide::types::UserId(0),
-                is_bot: true,
-                first_name: "Mock".to_owned(),
-                last_name: None,
-                username: None,
-                language_code: None,
-                is_premium: false,
-                added_to_attachment_menu: false,
-            })
-        }
-    }
-
-    impl MeGetters for Me {
-        fn user(&self) -> &teloxide::types::User {
-            &self.0
-        }
-    }
-
-    pub type MeFuture = Ready<Result<Me, Err>>;
 
     #[derive(Default)]
-    pub struct GetMe;
+    pub struct MockGetMe;
 
-    impl IntoFuture for GetMe {
+    impl IntoFuture for MockGetMe {
         type Output = <Self::IntoFuture as Future>::Output;
 
-        type IntoFuture = MeFuture;
+        type IntoFuture = MockMeFuture;
 
         fn into_future(self) -> Self::IntoFuture {
-            ready(Ok(Me::default()))
+            ready(Ok(MockMe::default()))
         }
     }
 }
