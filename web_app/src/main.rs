33c8e4bf11d62119d3ca2402d4ac239fe4813fea
Daniil Polyakov <arjentix@gmail.com>
2023-12-04 23:24:51 +0300

    Get web app expanding working

--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -4,12 +4,41 @@
 #![allow(clippy::empty_structs_with_brackets)]
 #![allow(clippy::same_name_method)]
 
+use js_sys::Reflect;
 use leptos::*;
+use wasm_bindgen::prelude::*;
+
+// #[wasm_bindgen(module = "https://telegram.org/js/telegram-web-app.js")]
+#[wasm_bindgen]
+extern "C" {
+    type WebApp;
+
+    #[wasm_bindgen(method)]
+    fn expand(this: &WebApp);
+}
 
 /// Main component.
-#[allow(clippy::missing_docs_in_private_items)]
+#[allow(
+    clippy::missing_docs_in_private_items,
+    clippy::panic,
+    clippy::expect_used
+)]
 #[component]
 fn App() -> impl IntoView {
+    let Some(window) = web_sys::window() else {
+        panic!("No window found");
+    };
+    let Ok(telegram) = Reflect::get(&window, &JsValue::from_str("Telegram")) else {
+        panic!("No Telegram found in window");
+    };
+    let Ok(web_app) = Reflect::get(&telegram, &JsValue::from_str("WebApp")) else {
+        panic!("No WebApp found in window.Telegram");
+    };
+
+    // `WebApp` is not a class, so checked casts like `dyn_into` fail.
+    let web_app = web_app.unchecked_into::<WebApp>();
+    web_app.expand();
+
     let (count, set_count) = create_signal(0_u32);
 
     view! {
