edf5a1134161661ba09bd6b3005a611a957c0ff5
Daniil Polyakov <arjentix@gmail.com>
2024-01-09 17:28:40 +0300

    Add password submission logic

--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -5,7 +5,7 @@
 #![allow(clippy::same_name_method)]
 
 use js_sys::Reflect;
-use leptos::*;
+use leptos::{html::Input, *};
 use wasm_bindgen::prelude::*;
 use web_sys::SubmitEvent;
 
@@ -26,9 +26,26 @@ extern "C" {
 
     /// Send data to the bot backend and close a [`WebApp`].
     ///
-    /// Only up to 4096 bytes of `data` will be sent.
-    #[wasm_bindgen(method)]
-    fn sendData(this: &WebApp, data: JsValue);
+    /// `data` must be no longer than 4096 bytes.
+    #[wasm_bindgen(method, catch)]
+    fn sendData(this: &WebApp, data: JsValue) -> Result<(), JsValue>;
+}
+
+/// Error during new password submission.
+#[derive(Debug, Clone, thiserror::Error, displaydoc::Display)]
+enum SubmissionError {
+    /// Failed to encrypt password
+    Encryption(#[from] telepass_crypto::Error),
+    /// Failed to serialize data: {0}
+    Serialization(String),
+    /// Failed to send data to the bot backend: {0}
+    Sending(String),
+}
+
+impl From<serde_json::Error> for SubmissionError {
+    fn from(e: serde_json::Error) -> Self {
+        Self::Serialization(e.to_string())
+    }
 }
 
 /// Main component.
@@ -50,24 +67,68 @@ fn App() -> impl IntoView {
     web_app.expand();
     web_app.enableClosingConfirmation();
 
+    let resource_name_element = create_node_ref::<Input>();
+    let password_element = create_node_ref::<Input>();
+    let master_password_element = create_node_ref::<Input>();
+
+    let (submission_result, set_submit_result) = create_signal(Ok(()));
+
     let on_submit = move |event: SubmitEvent| {
         event.prevent_default(); // Prevent page reload
-        web_app.sendData("Submitted".into());
+
+        let resource = resource_name_element()
+            .expect("No resource_name element")
+            .value();
+        let password = password_element().expect("No password element").value();
+        let master_password = master_password_element()
+            .expect("No master_password element")
+            .value();
+        set_submit_result(|| -> Result<(), SubmissionError> {
+            let telepass_crypto::EncryptionOutput {
+                encrypted_password,
+                salt,
+            } = telepass_crypto::encrypt(&password, &master_password)?;
+
+            let new_password = telepass_data_model::NewPassword {
+                resource,
+                encrypted_password,
+                salt,
+            };
+
+            // Telegram JS code checks some additional properties of the data (e.g. length),
+            // So it's easier to serialize it to JSON and send as a string rather than use
+            // something like `serde_wasm_bindgen`.
+            web_app
+                .sendData(serde_json::to_value(new_password)?.to_string().into())
+                .map_err(|err| SubmissionError::Sending(format!("{err:?}")))
+        }());
     };
 
     view! {
         <form on:submit=on_submit>
             <label for="resource_name">Resource name</label>
-            <input type="text" id="resource_name"/>
+            <input type="text" id="resource_name" node_ref=resource_name_element/>
 
             <label for="password">Password</label>
-            <input type="password" id="password"/>
+            <input type="password" id="password" node_ref=password_element/>
 
             <label for="master-password">Master Password</label>
-            <input type="password" id="master-password"/>
+            <input type="password" id="master-password" node_ref=master_password_element/>
 
             <input type="submit" value="Submit"/>
         </form>
+        <ErrorBoundary fallback=|errors| view! {
+            <div class = "error">
+                { move || {
+                    errors.get()
+                    .into_iter()
+                    .map(|(_, e)| view! { <p>{e.to_string()}</p>})
+                    .collect_view()
+                }}
+            </div>
+        }>
+            { submission_result }
+        </ErrorBoundary>
     }
 }
 
